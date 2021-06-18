216a7af240f680626dbc16819940a3d1bc6209e4
Rishabh Gautam <44278020+Rishabh7896@users.noreply.github.com>
2021-06-19 02:58:40 +0530

    Add files via upload

---
+++ Miscellaneous/BalancedParenthesis.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    stack<char>st;
+    for(int i=0;s[i]!='\0';i++)
+        if(s[i]=='(')
+           st.push('(');
+        else if(!st.empty())
+            st.pop();
+        else
+        {
+            cout<<"Invalid";
+            return 0;
+        }
+    if(st.empty())
+        cout<<"Valid";
+    else
+        cout<<"Invalid";
+    return 0;
+}

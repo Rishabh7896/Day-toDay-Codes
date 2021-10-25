1aa891513345f81d73731adf1a286d01c8ac64fc
Nitin Verma <44036947+The-Lost-One@users.noreply.github.com>
2021-10-26 02:14:33 +0530

    Update InsertionSort.cpp

--- Sorting-Algorithms/InsertionSort.cpp
+++ Sorting-Algorithms/InsertionSort.cpp
@@ -20,11 +20,15 @@ int main()
 {
 	int n;
 	cin>>n;
+	
 	vector<int>a(n);
+	
 	for(int i=0;i<n;i++)
 		cin>>a[i];
 	insertionsort(a,n);
+	
 	cout<<"Sorted Array: ";
+	
 	for(int i=0;i<n;i++)
 		cout<<a[i]<<" ";
 	return 0;
